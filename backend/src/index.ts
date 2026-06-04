3229b3fe189de7782a14ae26a3f82f99a42a819e
Eldor Khalikov <eldorabdukhalikov74@gmail.com>
2026-06-04 22:07:31 +0500

    fix: index.ts relative import paths

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
-import authRoutes from "../src/routes/auth.route.js";
-import questionRoute from "../src/routes/question-route.js";
+import authRoutes from "./routes/auth.route.js";
+import questionRoute from "./routes/question-route.js";
 
 const app = express();
 app.use(
@@ -17,8 +17,7 @@ app.use("/api", questionRoute);
 app.use((req, res) => {
     res.status(404).send({ message: "Bunday yo'l mavjud emas (404)" });
 });
-
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`🚀 Server ${PORT}-portda muvaffaqiyatli yondi`);
 });
