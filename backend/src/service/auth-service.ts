898bb1ddaa9d57091bec21b4aa8e24a0b748561b
Eldor Khalikov <eldorabdukhalikov74@gmail.com>
2026-06-07 16:06:38 +0500

    created user table for database

--- backend/src/service/auth-service.ts
+++ backend/src/service/auth-service.ts
@@ -16,6 +16,23 @@ const pool = new Pool({
         "postgresql://postgres:postgres@127.0.0.1:5432/template1",
     ssl: isLocal ? false : { rejectUnauthorized: false },
 });
+async function createTablesIfNotExist() {
+    const query = `
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            "fullName" VARCHAR(255)
+        );
+    `;
+    try {
+        await pool.query(query);
+        console.log("🚀 'users' jadvali muvaffaqiyatli tekshirildi/yaratildi!");
+    } catch (error) {
+        console.error("Jadval yaratishda xato yuz berdi:", error);
+    }
+}
+createTablesIfNotExist();
 
 const JWT_SECRET = process.env.JWT_SECRET || "Eli9391383$";
 
